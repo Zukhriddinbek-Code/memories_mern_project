38414588ab8b32b12e31eb362ce192b29e127254
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-09 16:45:58 +0900

    adding postmessage schema

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-const app = express();
+import postRoutes from "./routes/posts.js";
 
-// KnCcihyNGjLVe0nU
+const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,6 +14,8 @@ app.use(cors());
 const MONGO_URL =
   "mongodb+srv://zuhriddin-tech:KnCcihyNGjLVe0nU@cluster0.xcsxxsk.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
+
+app.use("/posts", postRoutes);
 
 mongoose
   .connect(MONGO_URL)
