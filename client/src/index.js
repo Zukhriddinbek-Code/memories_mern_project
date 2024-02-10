18cfd985e7563fb2f481a6c8cb20724307403d43
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-11 02:57:47 +0900

    adding redux and initializing

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
 import App from "./App";
+import postsReducer from "./reducers/postsReducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Automatically adds the thunk middleware and the Redux DevTools extension
+const store = configureStore({
+  // Automatically calls `combineReducers`
+  reducer: { posts: postsReducer },
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
