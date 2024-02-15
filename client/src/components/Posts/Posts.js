fdfda8088b60be4bb68b89c13e944aa894f6fc65
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-15 23:58:28 +0900

    creating a container grid for posts using material-ui/core

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, CircularProgress } from "@material-ui/core";}
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
@@ -11,8 +11,21 @@ const Posts = () => {
 
   console.log(posts);
 
-  return (
-!posts.length ? <CircularProgress/> : (<Grid></Grid>)
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
