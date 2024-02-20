2e3d47417e682b751c9fa5d0bfb1fa91e66125b8
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-20 15:12:33 +0900

    adding logic for deleting the post in backend /post/:id

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,11 @@
 import express from "express";
 
-import { getPosts, createPost, updatePost } from "../controllers/posts.js";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
@@ -11,5 +16,7 @@ router.post("/", createPost);
 
 //update thing existing documents
 router.patch("/:id", updatePost);
+
+router.delete("/:id", deletePost);
 
 export default router;
