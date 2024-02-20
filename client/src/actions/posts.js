c4404815a34c030349205cd9e66a7073b6204da0
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-20 15:19:09 +0900

    adding action creators, reducers and logic for deleting for frontend

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -30,3 +30,13 @@ export const updatePost = (id, updatePost) => async (dispatch) => {
     console.log(error);
   }
 };
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: "DELETE", payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
