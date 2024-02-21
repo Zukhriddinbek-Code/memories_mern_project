9d30c846ba26669f74402aa6d14b07fe6ef7656f
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-21 19:18:01 +0900

    adding .env variables for better exprience of security

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,12 +15,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const MONGO_URL =
-  "mongodb+srv://zuhriddin-tech:KnCcihyNGjLVe0nU@cluster0.xcsxxsk.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(MONGO_URL)
+  .connect(process.env.MONGO_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
   )
