cb1d47d0e829c877e0953c9c5e43e45c679927e3
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-24 23:40:57 +0900

    creating layout for auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,62 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+  TextField,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 import useStyles from "./styles";
 
 const Auth = () => {
   const classes = useStyles();
+  const isSignup = false;
 
-  return <div>Auth</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = () => {};
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+
+        <Typography variant="h5">{isSignup ? "Sing Up" : "Sign In"}</Typography>
+
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <TextField
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  xs={6}
+                />
+                <TextField
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  xs={6}
+                />
+              </>
+            )}
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Auth;
