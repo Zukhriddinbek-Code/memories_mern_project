ab50c304d5b9bbd126b6e3cb3c6afc3317418f3a
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-02-24 17:24:54 +0900

    seperating AppBar into Navbar.js

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { AppBar, Typography } from "@material-ui/core";
+import useStyles from "./styles";
+import memories from "../../images/memories.png";
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </div>
+    </AppBar>
+  );
+};
+
+export default Navbar;
